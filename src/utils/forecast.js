7f28a84fa5f340844dc7137ccf6a09eb6e1888f7
Steve Capie <steve@Steves-iMac.attlocal.net>
2021-08-17 06:39:27 -0700

    Add wind speed and direction to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (lat, lon, callback) => {
         } else if (body.error) {
             callback('Unable to find location!', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + '.  It is currently ' + body.current.temperature + ' degrees and feels like ' + body.current.feelslike + ' degrees.')
+            callback(undefined, 'Overall ' + body.current.weather_descriptions[0] + '.  It is currently ' + body.current.temperature + ' degrees and feels like ' + body.current.feelslike + ' degrees.  Winds: ' + body.current.wind_speed + 'mph ' + body.current.wind_dir)
         }
     })
 
